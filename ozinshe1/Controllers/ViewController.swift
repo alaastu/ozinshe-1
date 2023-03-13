3249699049155fa387704b224eb97425837262cf
Туйаара Алаас <tuiaara@MacBook-Pro-Tujaara-2.local>
2023-03-13 13:16:19 +0600

    Initial commit

---
+++ ozinshe1/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ozinshe1
+//
+//  Created by Туйаара Алаас on 08.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
